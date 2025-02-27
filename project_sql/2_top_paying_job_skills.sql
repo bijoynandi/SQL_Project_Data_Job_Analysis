85381cd69b2070a5a017557d908fd09c8b3e2f0c
Bijoy Nandi <bijoynandi31@gmail.com>
2025-02-28 01:00:00 +0530

    upload sql files

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,37 @@
+WITH 
+    top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        name AS company_name,
+        salary_year_avg
+    FROM
+        job_postings_fact AS j
+    JOIN
+        company_dim AS c
+    ON
+        j.company_id = c.company_id
+    WHERE
+        job_title_short = 'Data Analyst' 
+        AND job_location = 'Anywhere' 
+        AND salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT
+    top.*,
+    skills
+FROM 
+    top_paying_jobs AS top
+JOIN
+    skills_job_dim AS sd
+ON
+    top.job_id = sd.job_id
+JOIN
+    skills_dim AS s
+ON
+    sd.skill_id = s.skill_id
+ORDER BY
+    salary_year_avg DESC;
